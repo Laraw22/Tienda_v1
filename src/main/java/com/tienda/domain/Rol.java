520d96196b12f343f47be3430f2dbabab1a2f83b
HP EliteBook 840 g7 <HP EliteBook 840 g7@SHW>
2024-04-01 20:32:05 -0600

    commit semana 11

--- src/main/java/com/tienda/domain/Rol.java
+++ src/main/java/com/tienda/domain/Rol.java
@@ -22,6 +22,6 @@ public class Rol implements Serializable{
     private Long idRol;
     private String nombre;
     @Column(name= "id_usuario")
-    private Long Idusuario;
+    private Long IdUsuario;
     
 }
